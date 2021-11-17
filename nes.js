ee7574b98fdc7cfc273bd4a361ebbaaee9cc1560
Mae Mae <maemae@Maes-MacBook-Air.local>
2021-11-17 18:30:00 +0000

    no-animation

---
+++ nes.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+/*
+//// this expands all content in dialogue with one click
+const clicks = document.querySelectorAll(".click");
+const container = document.querySelector(".nes-container")
+clicks.forEach((current) => {
+    container.addEventListener("click", () => {
+        current.classList.replace("click", "expanded")
+    })
+})
+*/
+/*
+const click = document.querySelector(".click");
+const container = document.querySelector(".nes-container")
+container.addEventListener("click", () => {
+        click.classList.toggle("expanded")
+})
+*/
